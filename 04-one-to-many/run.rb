e1e76694479c383dafd0484dd09114d9c5272006
Eric Kim <hyeokjungkim@gmail.com>
2019-07-18 09:31:56 -0400

    adds lecture

---
+++ 04-one-to-many/run.rb
@@ -0,0 +1,9 @@
+require 'pry'
+# environment & application setup
+# requiring relative and necessary files to make our app communicate
+require "./user"
+require "./tweet"
+
+
+binding.pry
+puts 'done'
