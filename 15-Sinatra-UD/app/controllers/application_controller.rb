12336292751e2ccdab3bc578e36c6fbdf36a4321
Eric Kim <hyeokjungkim@gmail.com>
2019-08-07 14:08:54 -0400

    adds lecture

---
+++ 15-Sinatra-UD/app/controllers/application_controller.rb
@@ -0,0 +1,43 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+
+  get '/' do
+    erb :welcome
+  end
+
+  # INDEX
+  get '/students' do
+    # binding.pry
+    if params[:search_name]
+      # @students = Student.where("name like ?", "%#{params[:search_name]}%")
+      @students = Student.where(name: params[:search_name])
+    else
+      @students = Student.all
+    end
+    erb :index
+  end
+
+  # NEW
+  get '/students/new' do
+    erb :new
+  end
+
+  # CREATE
+  post '/students' do
+    @student = Student.create(params[:student])
+    redirect "/students/#{@student.id}"
+  end
+
+  # SHOW
+  get '/students/:id' do
+     @student = Student.find(params[:id])
+     erb :show
+  end
+
+end
